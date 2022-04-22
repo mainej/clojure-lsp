ef9fd615410bbc2543d95500efcdef589f440cdb
Jacob Maine <jacob.maine@gmail.com>
2022-04-22 12:02:53 -0700

    Shorten feedback of failing tests

--- cli/integration-test/entrypoint.clj
+++ cli/integration-test/entrypoint.clj
@@ -70,7 +70,7 @@
 
   (apply require namespaces)
 
-  (let [test-results (timeout 300000
+  (let [test-results (timeout (* 2 60 1000)
                               #(with-log-tail-report
                                  (apply t/run-tests namespaces)))]
 
